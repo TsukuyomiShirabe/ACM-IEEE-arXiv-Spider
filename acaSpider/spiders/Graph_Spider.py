f795891e279cf1b031fdc718001e641317036890
Wang YH at GuLab <yhwang16@163.com>
2021-03-10 16:47:38 +0800

    2021.03.10 Crawling Reference and Reference URL

--- acaSpider/spiders/Graph_Spider.py
+++ acaSpider/spiders/Graph_Spider.py
@@ -2,6 +2,7 @@ import scrapy
 from acaSpider.items import AcaspiderItem
 from scrapy.utils.project import get_project_settings
 from urllib.parse import urlparse
+from scrapy.http import HtmlResponse
 
 import regex
 import json
@@ -45,6 +46,10 @@ class GraphSpider(scrapy.Spider):
         item['abstract'] = []
         item['citation'] = []
 
+        
+
+        
+
         # Get domain of current webpage
         webpage_domain = self.dict_of_domains.get(urlparse(response.url).netloc, 'UnknownDomain')
 
@@ -59,21 +64,23 @@ class GraphSpider(scrapy.Spider):
             item[k].append(v)
             print(k,v)
         '''
+        print(json.dumps(dict_item))
+
         yield item
 
 
 
     def get_IEEE_item(self, response):
         # extract a json object that contains most information
-        script_list = response.xpath('//body/div[@id="LayoutWrapper"]/div/div/div/script').extract()
+        script_list = response.xpath('//body/div[@id="LayoutWrapper"]/div/div/div/script').getall()
         target_str = ''
         for script_item in script_list:
             start_idx = script_item.find('xplGlobal.document.metadata')
             if start_idx != -1:
                 target_str = script_item[start_idx:]
                 break
-        pattern = regex.compile(r'\{(?:[^{}]|(?R))*\}')
-        json_list = pattern.findall(target_str)
+        json_list = regex.findall(r'\{(?:[^{}]|(?R))*\}',target_str)
+        # print(json_list)
         json_item = json_list[0]
         json_to_dict = json.loads(json_item)
 
@@ -124,36 +131,60 @@ class GraphSpider(scrapy.Spider):
 
         # CITATION LIST
 
+
         # REFERENCE LIST
         reference_container_path = subresponse.xpath('//xpl-reference-panel/section/div[@class="document-ft-section-container"]/div/div[@class="reference-container"]')
-
         reference_list = []
-        for idx in xrange(len(reference_container_path)):
-            pass
-            # print(reference_container_path)
-            
-
-
+        for item in reference_container_path:
             reference_item = dict()
 
+            item_str = item.get()
+            item_html = HtmlResponse(url='reference_item', body=item_str, encoding='utf-8')
+            reference_info_str = item_html.xpath('//div/xpl-reference-item-migr/div/div/span[2]').xpath('string(.)').get()
+
+            reference_title = regex.findall(r'\"(.+?)\"', reference_info_str)
+            if len(reference_title) > 0:
+                reference_title = reference_title[0]
+            else:
+                reference_title = '[Reference TITLE Unknown]'
+            reference_item['reference_title'] = reference_title
+
+            # REFERENCE URL DICT
+            reference_url_dict = dict()
+            # REFERENCE URL, possibly contained in info string
+            if reference_info_str.find('[online]')>=0:
+                _url = regex.findall(r'(?:(?:http|ftp|https)\:\/\/)?(?:[\w_-]+(?:(?:\.[\w_-]+)+))(?:[\w.@?^=\%&:/~+#-]*[\w@?^=\%&/~+#-])?', reference_info_str)
+                # print('_url:', _url)
+                if len(_url) > 0:
+                    _url = _url[0]
+                else:
+                    _url = None
+                reference_url_dict['original_webpage'] = _url
+
+            # REFERENCE URL, class = 'stats-reference-link-crossRef' / 'stats-reference-link-googleScholar'
+            _url = item_html.xpath('//div/xpl-reference-item-migr/div/div/div[@class="ref-links-container stats-reference-links-container"]//a[@class="stats-reference-link-crossRef"]/@href').get()
+            reference_url_dict['cross_ref'] = _url
+            _url = item_html.xpath('//div/xpl-reference-item-migr/div/div/div[@class="ref-links-container stats-reference-links-container"]//a[@class="stats-reference-link-googleScholar"]/@href').get()
+            reference_url_dict['google_scholar'] = _url
+
+            reference_url_dict = {k:v for k,v in reference_url_dict.items() if v is not None}
+            reference_item['reference_url_dict'] = reference_url_dict
+
+            reference_url = '[Reference URL Unknown]'
+            # reference_url is the primary url in reference_url_dict
+            key_priority = ['original_webpage', 'cross_ref', 'google_scholar']
+
+            for key_str in key_priority:
+                reference_url = reference_url_dict.get(key_str, '[Reference URL Unknown]')
+                if reference_url != '[Reference URL Unknown]':
+                    break
+            reference_item['reference_url'] = reference_url
             reference_list.append(reference_item)
 
-
+        dict_item['reference_list'] = reference_list
 
         return dict_item
         
         
 
 
-
-    def merge_authors(self, au_list):
-        au_str = ''
-        for i in au_list:
-            au_str += i + ','
-        return au_str.strip(',')
-
-    def process4year(self, year):
-        return year[year.find(': ') + 1:].strip()
-
-    def process4citation(self, citation):
-        return citation[citation.find(' (')+2:citation.find(')')].strip()
